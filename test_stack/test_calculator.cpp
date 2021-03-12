f2dcebf09c9c5c12289c4dd17c510f3545a2d34e
AntonPoplavsky <poplavskyanton@mail.ru>
2021-03-13 00:12:05 +0300

    mp2-lab3-stack

--- test_stack/test_calculator.cpp
+++ test_stack/test_calculator.cpp
@@ -17,83 +17,83 @@ TEST(Calculator, can_get_postfix)
 TEST(Calculator, can_set_formula)
 {
 	Calculator calc;
-	string str = "5+5-4";
+	string str = "4-3";
 	ASSERT_NO_THROW(calc.SetExpression(str));
 }
 
 TEST(Calculator, can_set_a_string_to_calculator)
 {
 	Calculator calc;
-	string str = "5+5-4";
+	string str = "1+5-2";
 	ASSERT_NO_THROW(calc.SetFormula(str));
 }
 
 TEST(Calculator, can_check_wrong_brackets)
 {
 	Calculator calc;
-	string str = "5+(5-4";
+	string str = "8-(3+2";
 	ASSERT_ANY_THROW(calc.SetFormula(str));
 }
 
 TEST(Calculator, can_check_right_brackets)
 {
 	Calculator calc;
-	string str = "5+(5-4)";
+	string str = "8-(3+2)";
 	ASSERT_NO_THROW(calc.SetFormula(str));
 }
 
-TEST(Calculator, can_add_numbers)
+TEST(Calculator, can_add)
 {
 	Calculator calc;
-	calc.SetFormula("5+4");
+	calc.SetFormula("3+2");
 	calc.ToPostfix();
-	EXPECT_EQ(9, calc.CalcPostfix());
+	EXPECT_EQ(5, calc.CalcPostfix());
 }
 
-TEST(Calculator, can_subtract_numbers)
+TEST(Calculator, can_subtract)
 {
 	Calculator calc;
-	calc.SetFormula("5-4");
+	calc.SetFormula("3-2");
 	calc.ToPostfix();
 	EXPECT_EQ(1, calc.CalcPostfix());
 }
 
-TEST(Calculator, can_multiply_numbers)
+TEST(Calculator, can_multiply)
 {
 	Calculator calc;
-	calc.SetFormula("5*4");
+	calc.SetFormula("3*2");
 	calc.ToPostfix();
-	EXPECT_EQ(20, calc.CalcPostfix());
+	EXPECT_EQ(6, calc.CalcPostfix());
 }
 
-TEST(Calculator, can_divide_numbers)
+TEST(Calculator, can_divide)
 {
 	Calculator calc;
-	calc.SetFormula("8/2");
+	calc.SetFormula("3/2");
 	calc.ToPostfix();
-	EXPECT_EQ(4, calc.CalcPostfix());
+	EXPECT_EQ(1.5, calc.CalcPostfix());
 }
 
-TEST(Calculator, can_pow_numbers)
+TEST(Calculator, can_pow)
 {
 	Calculator calc;
-	calc.SetFormula("8^2");
+	calc.SetFormula("3^2");
 	calc.ToPostfix();
-	EXPECT_EQ(64, calc.CalcPostfix());
+	EXPECT_EQ(9, calc.CalcPostfix());
 }
 
-TEST(Calculator, can_do_all_operations_with_numbers_together)
+TEST(Calculator, can_do_all_operations_together)
 {
 	Calculator calc;
-	calc.SetFormula("1+2-5*4/5");
+	calc.SetFormula("5+4-9/3*2");
 	calc.ToPostfix();
-	EXPECT_EQ(-1, calc.CalcPostfix());
+	EXPECT_EQ(3, calc.CalcPostfix());
 }
 
 TEST(Calculator, cant_calculate_expression_with_wrong_number_of_operations)
 {
 	Calculator calc;
-	calc.SetFormula("5++5-4");
+	calc.SetFormula("3--2");
 	calc.ToPostfix();
 	EXPECT_ANY_THROW(calc.CalcPostfix());
 }
